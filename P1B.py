8112c09e917b9d47df1d194266b97dc9d3b4f3fc
Your Name <you@example.com>
2023-08-31 16:32:26 +0200

    PLOT SHIT

--- P1B.py
+++ P1B.py
@@ -12,32 +12,18 @@ seed = 57063
 system = SolarSystem(seed)
 mission = SpaceMission(seed)
 utils.check_for_newer_version()
-# @jit(nopython = True) #Optimalisering(?)
-# from mpl_toolkits import mplot3d  # Plotting
+#@jit(nopython = True) #Optimalisering(?)
 
 """Parametre"""
-L = 10e-7  # Bredde på boksen i meter
+L = 10e-6  # Bredde på boksen i meter
 T = 3000  # Gassens temperatur i kelvin
-N = 10000  # Antall partikler
+N = 100  # Antall partikler
 t_c = 10e-9  # Tid
 dt = 10e-12  # Tids intervall i s'
 t = np.arange(0, t_c, dt)  # Tidsarray definert vha Tid og tidssteg
-L = 10e-6  # Bredde på boksen i meter
 m_H2 = const.m_H2
 k_B = const.k_B
 MB = np.sqrt(const.k_B * T / const.m_H2)
-
-# fig = plt.figure()
-# ax = plt.axes(projection='3d')  #For 3d plotting av rakettmotoren
-# ax.plot3D([0,L], [0,0], [0,0], 'green')    #Lager en yttre firkant på xy-planet
-# ax.plot3D([L,L], [0,L], [0,0], 'green')
-# ax.plot3D([0,0], [0,L], [0,0], 'green')
-# ax.plot3D([0,L], [L,L], [0,0], 'green')
-
-# ax.plot3D([0.25*L,0.75*L], [0.25*L,0.25*L], [0,0], 'green')    #Lager en indre firkant på xy-planet (utgangshull)
-# ax.plot3D([0.25*L,0.75*L], [0.75*L,0.75*L], [0,0], 'green')
-# ax.plot3D([0.25*L,0.25*L], [0.25*L,0.75*L], [0,0], 'green')
-# ax.plot3D([0.75*L,0.75*L], [0.25*L,0.75*L], [0,0], 'green')
 
 # print('My system has a {:g} solar mass star with a radius of {:g} kilometers.'
 #      .format(system.star_mass, system.star_radius))
@@ -51,7 +37,6 @@ MB = np.sqrt(const.k_B * T / const.m_H2)
 
 """Kode for 1B og 1C."""
 
-
 def simulate_small_engine(
         
     npb,
@@ -61,7 +46,6 @@ def simulate_small_engine(
     tuppel: (vel, average_pressure, average_energy, F, fuel_cons).
     """
     a = []  # Skal telle farten til partiklene som slipper ut
-    # nr = []  # Bare til plotting underveis
     rows = 3  # For vectors
     cols = npb
     pos = L * np.random.rand(rows, cols)  # Particle positions
@@ -102,29 +86,8 @@ def simulate_small_engine(
                         pos[i][m] = L * np.random.rand()
                     pos[2][m] = L
 
-        #             if z2[m] not in nr:  # Brukes til plotting
-        #                 nr.append(z2[m])
-        # z2 = list(z2)
-        # x1 = list(x1)
-        # x2 = list(x2)
-        # y1 = list(y1)
-        # y2 = list(y2)
-        # for i in range(len(nr)):  # For plotting
-        #     if nr[i] in z2:
-        #         z2.remove(nr[i])
-        #     if nr[i] in x1:
-        #         x1.remove(nr[i])
-        #     if nr[i] in x2:
-        #         x2.remove(nr[i])
-        #     if nr[i] in y1:
-        #         y1.remove(nr[i])
-        #     if nr[i] in y2:
-        #         y2.remove(nr[i])
-
         vel[0][x1] = -vel[0][x1]
-        vel[0][x2] = -vel[0][
-            x2
-        ]  # Elastisk støt ved å snu farten til det motsatte i en gitt retning
+        vel[0][x2] = -vel[0][x2]  # Elastisk støt ved å snu farten til det motsatte i en gitt retning
         vel[1][y1] = -vel[1][y1]
         vel[1][y2] = -vel[1][y2]
         vel[2][z1] = -vel[2][z1]
@@ -177,7 +140,7 @@ def plot_velocity_distribution(npb, bins=30):
     )
     ax1.plot(
         x_axis,
-        norm.pdf(x_axis, loc=mean, scale=mean),
+        norm.pdf(x_axis, loc=mean, scale=std),
         color="red",
         label="MB-dist",
     )
@@ -213,6 +176,80 @@ def plot_velocity_distribution(npb, bins=30):
     ax3.legend(loc="upper left")
     fig.suptitle("Simulated velocity of our particles compared to Maxwell-Boltzmann")
     plt.show()
-def plot_small_engine():
-# simulate_small_engine(N)
-# plot_velocity_distribution(N)
+def plot_small_engine(npb = 100, time = 100):
+    from mpl_toolkits import mplot3d  # Plotting
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')  #For 3d plotting av rakettmotoren
+    ax.plot3D([0,L], [0,0], [0,0], 'green')    #Lager en yttre firkant på xy-planet
+    ax.plot3D([L,L], [0,L], [0,0], 'green')
+    ax.plot3D([0,0], [0,L], [0,0], 'green')
+    ax.plot3D([0,L], [L,L], [0,0], 'green')
+
+    ax.plot3D([0.25*L,0.75*L], [0.25*L,0.25*L], [0,0], 'green')    #Lager en indre firkant på xy-planet (utgangshull)
+    ax.plot3D([0.25*L,0.75*L], [0.75*L,0.75*L], [0,0], 'green')
+    ax.plot3D([0.25*L,0.25*L], [0.25*L,0.75*L], [0,0], 'green')
+    ax.plot3D([0.75*L,0.75*L], [0.25*L,0.75*L], [0,0], 'green')
+
+    nr = []  # Bare til plotting underveis
+    rows = 3  # For vectors
+    cols = npb
+    pos = L * np.random.rand(rows, cols)  # Particle positions
+    mean = 0
+    std = MB
+    vel = np.random.normal(
+        loc=mean, scale=std, size=(rows, cols)
+    )  # loc = mean, scale = standard deviation(std)
+    for i in range(
+        len(vel)
+    ):  # Sørger for at ingen hastigheter er negative(Sjelden feil)
+        vel_0 = np.where(vel[i] == 0)[0]
+        vel[i][vel_0] = vel[i - 1][vel_0]
+
+    for m in range(time):  # tidssteg
+        pos += dt * vel  # Euler cromer
+
+        x1 = np.where(pos[0] >= L)[0]  # Ser etter kollisjoner for x
+        x2 = np.where(pos[0] <= 0)[0]
+        y1 = np.where(pos[1] >= L)[0]  # Ser etter kollisjoner for y
+        y2 = np.where(pos[1] <= 0)[0]
+        z1 = np.where(pos[2] >= L)[0]  # Ser etter kollisjoner for z
+        z2 = np.where(pos[2] <= 0)[0]
+
+        for m in range(len(z2)): #Sjekker om kollisjonene for z2(xy-planet) 
+            if (L / 4 < pos[0][z2[m]] < (3 / 4) * L):  #egentlig er i 
+                if L / 4 < pos[1][z2[m]] < (3 / 4) * L: #utgangshullet
+                    for i in range(2):  #Flytter partikkelen til en uniformt 
+                        pos[i][m] = L * np.random.rand() #fordelt posisjon på 
+                    pos[2][m] = L   #toppen av boksen, med samme vel.
+                    if z2[m] not in nr:  # Brukes til plotting
+                        nr.append(z2[m])   
+        # z2 = list(z2)
+        # x1 = list(x1)
+        # x2 = list(x2)
+        # y1 = list(y1)
+        # y2 = list(y2)
+        # for i in range(len(nr)):  # For plotting
+        #     if nr[i] in z2:       # Av at partiklene fyker ut av boksen
+        #         z2.remove(nr[i])
+        #     if nr[i] in x1:
+        #         x1.remove(nr[i])
+        #     if nr[i] in x2:
+        #         x2.remove(nr[i])
+        #     if nr[i] in y1:
+        #         y1.remove(nr[i])
+        #     if nr[i] in y2:
+        #         y2.remove(nr[i])
+
+        vel[0][x1] = -vel[0][x1]
+        vel[0][x2] = -vel[0][x2]  # Elastisk støt ved å snu 
+        vel[1][y1] = -vel[1][y1]  # farten til det motsatte i en gitt retning
+        vel[1][y2] = -vel[1][y2]    
+        vel[2][z1] = -vel[2][z1]
+        vel[2][z2] = -vel[2][z2]
+
+        ax.scatter(pos[0], pos[1], pos[2])  # Plotter rakettmotoren
+    
+    plt.show()
+#simulate_small_engine(N)
+#plot_velocity_distribution(N)
+#plot_small_engine()
--- Slett.py
+++ Slett.py
@@ -1,6 +1,16 @@
 import numpy as np
-a = [1,2,3,5,10] 
-b = [2,5,6,78,8,9,10]
-
-for i in range(2):
-    print(b)
+import matplotlib.pyplot as plt
+def efieldq(q1,r,r1):
+    # Input: charge q1 in Coulomb
+    #        r: position to find field (in 1,2 or 3 dimensions) in meters
+    #        r1: position of charge q1 in meters
+    # Output: electric field E at position r in N/C
+    r = np.array(r)
+    r1 = np.array(r1)
+    Rvec = r - r1
+    Rnorm = np.sqrt(sum(r**2 + r1**2))
+    epsilon0 = 8.854187817e-12
+    E = Rvec * (q1 / (4*np.pi*epsilon0)) * (1/ Rnorm**3)
+    return E
+E = efieldq(1e-6, (0,0), (1,0))
+print(E)
