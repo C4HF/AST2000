f47ee54addcba0f2dbbc833dbeab28bf94f6ba72
C4HF <adam.falchenberg@gmail.com>
2023-08-31 18:14:54 +0200

    added function to calculate burned fuel boost

--- P1B.py
+++ P1B.py
@@ -12,18 +12,21 @@ seed = 57063
 system = SolarSystem(seed)
 mission = SpaceMission(seed)
 utils.check_for_newer_version()
-#@jit(nopython = True) #Optimalisering(?)
+# @jit(nopython = True) #Optimalisering(?)
 
 """Parametre"""
-L = 10e-6  # Bredde på boksen i meter
+L = 10e-7  # Bredde på boksen i meter
 T = 3000  # Gassens temperatur i kelvin
-N = 100  # Antall partikler
+N = 10**4  # Antall partikler
 t_c = 10e-9  # Tid
 dt = 10e-12  # Tids intervall i s'
 t = np.arange(0, t_c, dt)  # Tidsarray definert vha Tid og tidssteg
 m_H2 = const.m_H2
 k_B = const.k_B
 MB = np.sqrt(const.k_B * T / const.m_H2)
+dry_rocket_mass = mission.spacecraft_mass
+crosssection_rocket = mission.spacecraft_area
+escape_velocity = 11882
 
 # print('My system has a {:g} solar mass star with a radius of {:g} kilometers.'
 #      .format(system.star_mass, system.star_radius))
@@ -37,8 +40,8 @@ MB = np.sqrt(const.k_B * T / const.m_H2)
 
 """Kode for 1B og 1C."""
 
+
 def simulate_small_engine(
-        
     npb,
 ):
     """Funksjonen simulerer bevegelsen til partiklene i en liten boks.
@@ -74,9 +77,9 @@ def simulate_small_engine(
 
         for m in range(len(z2)):
             if (
-                L / 4 < pos[0][z2[m]] < (3 / 4) * L
+                L / 8 < pos[0][z2[m]] < (7 / 8) * L
             ):  # Sjekker om kollisjonene for z2(xy-planet) egentlig er i utgangshullet
-                if L / 4 < pos[1][z2[m]] < (3 / 4) * L:
+                if L / 8 < pos[1][z2[m]] < (7 / 8) * L:
                     a.append(
                         vel[2][z2[m]]
                     )  # Lagrer farten til partiklene som forsvinner ut. Kan brukes til beregninger
@@ -87,7 +90,9 @@ def simulate_small_engine(
                     pos[2][m] = L
 
         vel[0][x1] = -vel[0][x1]
-        vel[0][x2] = -vel[0][x2]  # Elastisk støt ved å snu farten til det motsatte i en gitt retning
+        vel[0][x2] = -vel[0][
+            x2
+        ]  # Elastisk støt ved å snu farten til det motsatte i en gitt retning
         vel[1][y1] = -vel[1][y1]
         vel[1][y2] = -vel[1][y2]
         vel[2][z1] = -vel[2][z1]
@@ -123,6 +128,8 @@ def simulate_small_engine(
     F = -P / t_c  # F = mv / dt
 
     return vel, average_pressure, average_energy, F, fuel_cons
+
+
 def plot_velocity_distribution(npb, bins=30):
     """Funksjonen kaller på simulate_small_engine funksjonen og danner et
     subplot som sammenligner den simulerte farten med Maxwell-Boltzmann fordelingen.
@@ -176,19 +183,24 @@ def plot_velocity_distribution(npb, bins=30):
     ax3.legend(loc="upper left")
     fig.suptitle("Simulated velocity of our particles compared to Maxwell-Boltzmann")
     plt.show()
-def plot_small_engine(npb = 100, time = 100):
-    from mpl_toolkits import mplot3d  # Plotting
-    fig = plt.figure()
-    ax = plt.axes(projection='3d')  #For 3d plotting av rakettmotoren
-    ax.plot3D([0,L], [0,0], [0,0], 'green')    #Lager en yttre firkant på xy-planet
-    ax.plot3D([L,L], [0,L], [0,0], 'green')
-    ax.plot3D([0,0], [0,L], [0,0], 'green')
-    ax.plot3D([0,L], [L,L], [0,0], 'green')
 
-    ax.plot3D([0.25*L,0.75*L], [0.25*L,0.25*L], [0,0], 'green')    #Lager en indre firkant på xy-planet (utgangshull)
-    ax.plot3D([0.25*L,0.75*L], [0.75*L,0.75*L], [0,0], 'green')
-    ax.plot3D([0.25*L,0.25*L], [0.25*L,0.75*L], [0,0], 'green')
-    ax.plot3D([0.75*L,0.75*L], [0.25*L,0.75*L], [0,0], 'green')
+
+def plot_small_engine(npb=100, time=100):
+    from mpl_toolkits import mplot3d  # Plotting
+
+    fig = plt.figure()
+    ax = plt.axes(projection="3d")  # For 3d plotting av rakettmotoren
+    ax.plot3D([0, L], [0, 0], [0, 0], "green")  # Lager en yttre firkant på xy-planet
+    ax.plot3D([L, L], [0, L], [0, 0], "green")
+    ax.plot3D([0, 0], [0, L], [0, 0], "green")
+    ax.plot3D([0, L], [L, L], [0, 0], "green")
+
+    ax.plot3D(
+        [0.25 * L, 0.75 * L], [0.25 * L, 0.25 * L], [0, 0], "green"
+    )  # Lager en indre firkant på xy-planet (utgangshull)
+    ax.plot3D([0.25 * L, 0.75 * L], [0.75 * L, 0.75 * L], [0, 0], "green")
+    ax.plot3D([0.25 * L, 0.25 * L], [0.25 * L, 0.75 * L], [0, 0], "green")
+    ax.plot3D([0.75 * L, 0.75 * L], [0.25 * L, 0.75 * L], [0, 0], "green")
 
     nr = []  # Bare til plotting underveis
     rows = 3  # For vectors
@@ -215,14 +227,14 @@ def plot_small_engine(npb = 100, time = 100):
         z1 = np.where(pos[2] >= L)[0]  # Ser etter kollisjoner for z
         z2 = np.where(pos[2] <= 0)[0]
 
-        for m in range(len(z2)): #Sjekker om kollisjonene for z2(xy-planet) 
-            if (L / 4 < pos[0][z2[m]] < (3 / 4) * L):  #egentlig er i 
-                if L / 4 < pos[1][z2[m]] < (3 / 4) * L: #utgangshullet
-                    for i in range(2):  #Flytter partikkelen til en uniformt 
-                        pos[i][m] = L * np.random.rand() #fordelt posisjon på 
-                    pos[2][m] = L   #toppen av boksen, med samme vel.
+        for m in range(len(z2)):  # Sjekker om kollisjonene for z2(xy-planet)
+            if L / 4 < pos[0][z2[m]] < (3 / 4) * L:  # egentlig er i
+                if L / 4 < pos[1][z2[m]] < (3 / 4) * L:  # utgangshullet
+                    for i in range(2):  # Flytter partikkelen til en uniformt
+                        pos[i][m] = L * np.random.rand()  # fordelt posisjon på
+                    pos[2][m] = L  # toppen av boksen, med samme vel.
                     if z2[m] not in nr:  # Brukes til plotting
-                        nr.append(z2[m])   
+                        nr.append(z2[m])
         # z2 = list(z2)
         # x1 = list(x1)
         # x2 = list(x2)
@@ -241,15 +253,60 @@ def plot_small_engine(npb = 100, time = 100):
         #         y2.remove(nr[i])
 
         vel[0][x1] = -vel[0][x1]
-        vel[0][x2] = -vel[0][x2]  # Elastisk støt ved å snu 
+        vel[0][x2] = -vel[0][x2]  # Elastisk støt ved å snu
         vel[1][y1] = -vel[1][y1]  # farten til det motsatte i en gitt retning
-        vel[1][y2] = -vel[1][y2]    
+        vel[1][y2] = -vel[1][y2]
         vel[2][z1] = -vel[2][z1]
         vel[2][z2] = -vel[2][z2]
 
         ax.scatter(pos[0], pos[1], pos[2])  # Plotter rakettmotoren
-    
+
     plt.show()
-#simulate_small_engine(N)
-#plot_velocity_distribution(N)
-#plot_small_engine()
+
+
+"""Kode 1D"""
+
+
+def calculate_needed_fuel(thrust, fuel_consumption, initial_rocket_mass, speed_boost):
+    """Funksjonen regner ut hvor mye drivstoff vi trenger for å øke hastigheten med en ønsket mengde.
+    Tar inn variabler (thrust, fuel_consumption, initial_rocket_mass, speed_boost) og returnerer
+    fuel_consumed."""
+    dt = 10
+    start_speed = 0
+    total_time = 0
+    while start_speed < speed_boost:
+        start_speed += (thrust / initial_rocket_mass) * dt
+        initial_rocket_mass -= fuel_consumption * dt
+        total_time += dt
+    fuel_consumed = total_time * fuel_consumption
+    return fuel_consumed
+
+
+## Utregning av total thrust, total fuel-constant og total rocket mass
+(vel, average_pressure, average_energy, F, fuel_cons) = simulate_small_engine(N)
+estimated_fuel_weight = 4500
+number_of_engines = crosssection_rocket / (L**2)
+rocket_total_thrust = number_of_engines * F
+total_fuel_constant = fuel_cons * number_of_engines
+wet_rocket_mass = dry_rocket_mass + estimated_fuel_weight
+
+print("Number of engines:")
+print(number_of_engines)
+print("Total thrust per second in Newton:")
+print(rocket_total_thrust)
+print("Fuel consumed per small engine per second:")
+print(fuel_cons)
+print("Total fuel used per second of entire rocket:")
+print(total_fuel_constant)
+print("Total weight rocket")
+print(wet_rocket_mass)
+
+needed_fuel = calculate_needed_fuel(
+    rocket_total_thrust, total_fuel_constant, wet_rocket_mass, escape_velocity
+)
+print("Total burned fuel to perform velocity-boost")
+print(needed_fuel)
+
+# simulate_small_engine(N)
+# plot_velocity_distribution(N)
+# plot_small_engine()
