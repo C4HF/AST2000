82956163be65e10e13c7703ef495778208d4cee1
C4HF <adam.falchenberg@gmail.com>
2023-10-13 15:43:15 +0200

    ryddet

--- p4.py
+++ p4.py
@@ -5,8 +5,6 @@ import ast2000tools.constants as const
 import ast2000tools.utils as utils
 from ast2000tools.space_mission import SpaceMission
 from ast2000tools.solar_system import SolarSystem
-from scipy.stats import norm
-import numba as nb
 from numba import njit
 import math
 from P1B import Engine
