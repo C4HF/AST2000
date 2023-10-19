29a5ff620c3ea62d38b9fbeaa50e8b1b8eab8204
Your Name <you@example.com>
2023-10-19 09:55:55 +0200

    Create p6

--- p6.py
+++ p6.py
@@ -1,1 +1,34 @@
+########## Ikke kodemal #############################
+import numpy as np
+import matplotlib.pyplot as plt
+import ast2000tools.constants as const
+import ast2000tools.utils as utils
+from ast2000tools.space_mission import SpaceMission
+from ast2000tools.solar_system import SolarSystem
+from scipy.stats import norm
+import numba as nb
+from numba import njit
+import math
+from P1B import Engine
+from P2 import simulate_orbits
+import h5py
+from part3 import generalized_launch_rocket
+from PIL import Image
+
+utils.check_for_newer_version()
+
+np.random.seed(10)
+seed = 57063
+system = SolarSystem(seed)
+mission = SpaceMission(seed)
+Au = 149597870700  # Meters
+SM = 1.9891 * 10 ** (30)  # Solar masses in kg
+sec_per_year = 60 * 60 * 24 * 365
+c_AU = 63239.7263  # Speed of light in Au/yr
+c = const.c * (sec_per_year / Au)  # Speed of light in Au/yr
+G_AU = 4 * (np.pi) ** 2  # Gravitational constant using AU
+G = 6.6743 * (10 ** (-11))  # Gravitational constant
+lambda_0 = 656.3  # wavelength of the Hα spectral line from restframe in nanometers
+m_H2 = const.m_H2
+k_B = const.k_B
 
