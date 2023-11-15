efb4e2f2cbbdec0521841a1244544108e602e300
C4HF <adam.falchenberg@gmail.com>
2023-11-15 18:45:26 +0100

    fixing lander code

--- P7.py
+++ P7.py
@@ -33,8 +33,13 @@ G = 6.6743 * (10 ** (-11))  # Gravitational constant
 m_H2 = const.m_H2
 k_B = const.k_B
 
+
 lander_mass = mission.lander_mass  # 90.0 kg
 lander_area = mission.lander_area  # 0.3 m^2
+planet_mass = system.masses[1] * SM  # kg
+planet_radius = system.radii[1] * 1000  # radius in meters
+g0 = (G * planet_mass) / planet_radius**2  # gravitational acceleration at surface
+mu = 
 
 # def Landing(inital_time, inital_position, inital_velocity, simulation_time):
 #     air_resistance =
@@ -78,24 +83,6 @@ def landing_trajectory(
     x_vel_arr[0] = initial_vel[0]  # Setting initial x-velocity
     y_vel_arr[0] = initial_vel[1]  # Setting initial y-velocity
 
-    # interpolated_orbits = [[], []]  # Empty list to be filled with interpolated orbits
-
-    # # Loop to interpolate simulated orbits to fit shape of time_array.
-    # for orbit in orbits:
-    #     f = interpolate.interp1d(
-    #         orbit[0], orbit[1:3], axis=-1
-    #     )  # Using scipy.interpolate to interpolate orbits
-    #     t = time_array
-    #     x, y = f(
-    #         t
-    #     )  # Using interpolation-function to calculate planet-postions during the timesteps in time_array
-    #     interpolated_orbits[0].append(x)  # adding to list
-    #     interpolated_orbits[1].append(y)  # adding to list
-
-    # r_planets = np.array(
-    #     interpolated_orbits
-    # )  # Turning interpolated_orbits-list to array
-
     r_rocket = np.array(
         [[x_pos_arr], [y_pos_arr]]
     )  # Empty pos-array for rocket position, to be filled with values from simulation
@@ -103,50 +90,35 @@ def landing_trajectory(
         [[x_vel_arr], [y_vel_arr]]
     )  # Empty vel-array for rocket position, to be filled with values from simulation
 
-    G = 4 * (np.pi) ** 2  # Gravitational constant for Au
-    a_sun = ((-G * star_mass) * r_rocket[:, :, 0]) / (
+    a_planet = ((-G * planet_mass) * r_rocket[:, :, 0]) / (
         np.sqrt(np.sum(r_rocket[:, :, 0] ** 2))
-    ) ** 3  # Sets initial acceleration from sun according to N.2 law
-    a_planets = -np.sum(
-        (
-            (
-                G
-                * system.masses
-                * np.sqrt(np.sum((r_rocket[:, :, 0] - r_planets[:, :, 0]) ** 2))
-            )
-        )
-        / (np.sqrt(np.sum((r_rocket[:, :, 0] - r_planets[:, :, 0]) ** 2)) ** 3)
-    )  # Sets initial acceleration from planets according to N.2 law
-    acc_old = a_sun + a_planets
+    ) ** 3  # Sets initial acceleration from planet according to N.2 law
+    acc_old = a_planet
+
     # Leapfrog-loop
     for i in range(0, len(time_array) - 1):
+        rho = 1  # expression for rho
+        v_terminal = np.sqrt(
+            G
+            * (2 * lander_mass * planet_mass * r_rocket[i])
+            / (rho * lander_area * np.linalg.norm(r_rocket[i]) ** 3)
+        )
         r_rocket[:, :, i + 1] = (
             r_rocket[:, :, i]
             + v_rocket[:, :, i] * time_step
             + (acc_old * time_step**2) / 2
         )  # Rocket pos at time i+1
 
-        a_sun = (
-            (-G * star_mass)
+        a_planet = (
+            (-G * planet_mass)
             * r_rocket[:, :, i + 1]
             / (np.sqrt(np.sum(r_rocket[:, :, i + 1] ** 2)) ** 3)
         )  # Sets initial acceleration from sun according to N.2 law
-        a_planets = -np.sum(
-            (
-                (
-                    G
-                    * system.masses
-                    * np.sqrt(np.sum((r_rocket[:, :, i] - r_planets[:, :, i]) ** 2))
-                )
-            )
-            / (np.sqrt(np.sum((r_rocket[:, :, i] - r_planets[:, :, i]) ** 2)) ** 3)
-        )  # Sets initial acceleration from planets according to N.2 law
-        acc_new = (
-            a_sun + a_planets
-        )  # Setting new acceleration to calculate velocity change
+
+        acc_new = a_planet  # Setting new acceleration to calculate velocity change
         v_rocket[:, :, i + 1] = (
             v_rocket[:, :, i] + (1 / 2) * (acc_old + acc_new) * time_step
         )  # Calculating velocty of rocket in timestep i+1
         acc_old = acc_new
 
-    return time_array, r_rocket, v_rocket, r_planets
+    return time_array, r_rocket, v_rocket
